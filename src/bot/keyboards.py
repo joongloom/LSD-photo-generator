bc1bfb8b38eaefbb673838618ddd33394fe09919
krivitoshi <m4x1m.m15h1n@yandex.ru>
2025-11-02 09:54:04 +0300

    v2

--- src/bot/keyboards.py
+++ src/bot/keyboards.py
@@ -2,7 +2,15 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_settings_kb():
     kb = [
-        [KeyboardButton(text="Low"), KeyboardButton(text="Medium"), KeyboardButton(text="High")],
-        [KeyboardButton(text="🎲 Surprise Me (Random)")]
+        [
+            KeyboardButton(text="Art"),
+            KeyboardButton(text="Trip"),
+            KeyboardButton(text="Deep")
+        ],
+        [
+            KeyboardButton(text="Low"),
+            KeyboardButton(text="Medium"),
+            KeyboardButton(text="High")
+        ]
     ]
     return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
